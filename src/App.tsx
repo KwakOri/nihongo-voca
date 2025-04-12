6b3059e7010aa33177782b074ca61db292565445
kwakjaehun <jh5414092@icloud.com>
2025-04-12 19:39:26 +0900

    Feat: 홈페이지 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="w-full h-full flex flex-col gap-4">
+    <div className="w-full h-full flex flex-col gap-4 justify-center items-center">
       <h1 className="text-white text-xl">NIHONGO VOCA</h1>
-      <NavLink to="levels">
-        <p className="bg-[#2d2d2d] w-full h-20 rounded flex justify-center items-center text-white">
-          공부하러 가기
-        </p>
+
+      <NavLink
+        to="levels"
+        className="bg-[#2d2d2d] w-full h-20 rounded flex justify-center items-center text-white"
+      >
+        공부하러 가기
       </NavLink>
     </div>
   );
--- src/pages/levels/Levels.tsx
+++ src/pages/levels/Levels.tsx
@@ -4,6 +4,16 @@ const Levels = () => {
   const levels = [1, 2, 3, 4, 5];
   return (
     <div className="flex flex-col gap-4">
+      <div className="w-full h-10 grid grid-cols-3 gap-2">
+        <NavLink
+          to="/"
+          className={
+            "rounded bg-[#2d2d2d] w-full h-full flex justify-center items-center text-white text-xs"
+          }
+        >
+          뒤로가기
+        </NavLink>
+      </div>
       {levels.map((level, index) => (
         <NavLink to={`${level}`} key={index}>
           <p className="w-full h-20 rounded bg-[#2d2d2d] flex justify-center items-center text-white text-xl">
