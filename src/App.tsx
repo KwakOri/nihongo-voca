e62ecc2fc5fedebdba2625a56520c918dd1f5bf1
kwakjaehun <jh5414092@icloud.com>
2025-04-12 17:31:32 +0900

    Feat: home에서의 진입로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { NavLink } from "react-router";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>hello this is Home</h1>
-    </>
+    <div className="w-full h-full flex flex-col gap-4">
+      <h1 className="text-white text-xl">NIHONGO VOCA</h1>
+      <NavLink to="levels">
+        <p className="bg-[#2d2d2d] w-full h-20 rounded flex justify-center items-center text-white">
+          공부하러 가기
+        </p>
+      </NavLink>
+    </div>
   );
 }
 
